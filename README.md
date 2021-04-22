52de74cec37cfe225c5573bfb85a69caed61f156
Symbioquine <symbioquine@gmail.com>
2021-04-22 12:11:14 -0700

    Initial commit - basic money asset with no custom fields

---
+++ README.md
@@ -0,0 +1,3 @@
+# farmOS_money_asset
+
+A 'money asset' module for farmOS - enables the tracking of accounts or balances using the standard farmOS 2.x quantity/inventory model.
---
+++ farmos_money_asset/src/Plugin/Asset/AssetType/Money.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Drupal\farmos_money_asset\Plugin\Asset\AssetType;
+
+use Drupal\farm_entity\Plugin\Asset\AssetType\FarmAssetType;
+
+/**
+ * Provides the money asset type.
+ *
+ * @AssetType(
+ *   id = "money",
+ *   label = @Translation("Money"),
+ * )
+ */
+class Money extends FarmAssetType {
+
+}
+
